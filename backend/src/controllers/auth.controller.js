5ebd998ccd879334f553a6e5035e0245407631e6
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-03 22:37:36 +0530

    Done with logout route in auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -94,5 +94,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  
+    try {
+        res.clearCookie("jwt");
+        res.status(200).json({ message: "User logged out successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 };  
