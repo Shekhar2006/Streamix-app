54db4f5c610daffdc9db144c65b7eae6861a6995
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-03 20:40:49 +0530

    done with signup route and generate jwt token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,56 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+
+
 export const signup = async (req, res) => {
-    res.send("signup");
+    const { fullName, email, password } = req.body;
+    try {
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const index = Math.floor(Math.random() * 100);
+        const randomAvatar = `https://avatar.iran.liara.run/public/${index}.png`;
+
+
+        const newUser = new User({ fullName, email, password, profilePic: randomAvatar });
+        await newUser.save();
+
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie("jwt", token, { 
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+            httpOnly: true, 
+            secure: process.env.NODE_ENV === "production", 
+            sameSite: "strict" 
+        });
+
+        res.status(201).json({ message: "User created successfully", newUser});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 };
 
 export const login = async (req, res) => {
     res.send("login");
-    
+
 };
 
 export const logout = async (req, res) => {
