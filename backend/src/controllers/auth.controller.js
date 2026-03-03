3c74ee082f7ecbe5c0e556c6f6802bae9662dbc5
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-03 22:37:04 +0530

    Done with login route in auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 import 'dotenv/config';
 
 
@@ -33,14 +34,14 @@ export const signup = async (req, res) => {
         await newUser.save();
 
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-        res.cookie("jwt", token, { 
-            maxAge: 7 * 24 * 60 * 60 * 1000, 
-            httpOnly: true, 
-            secure: process.env.NODE_ENV === "production", 
-            sameSite: "strict" 
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict"
         });
 
-        res.status(201).json({ message: "User created successfully", newUser});
+        res.status(201).json({ message: "User created successfully", newUser });
 
     } catch (error) {
         console.log(error);
@@ -49,10 +50,49 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    res.send("login");
+   const {email , password} = req.body;
+   try {
+
+    if(!email || !password){
+        return res.status(400).json({message : "All fields are required"});
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict"
+    });
+
+    res.status(201).json({ message: "User logged in successfully", existingUser });
+    
+   } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+   }
 
 };
 
 export const logout = async (req, res) => {
-    res.send("logout");
+  
 };  
