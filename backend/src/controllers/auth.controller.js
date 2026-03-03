0c9fc00c4e5b3f8a8ea32f29b002bcc19ec03620
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-03 18:56:07 +0530

    Done with basic setup of auth controller and auth routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+    res.send("signup");
+};
+
+export const login = async (req, res) => {
+    res.send("login");
+    
+};
+
+export const logout = async (req, res) => {
+    res.send("logout");
+};  
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 // configuration 
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use("/api/auth" , authRoutes);
 
 // server 
 app.listen(process.env.PORT, () => {
