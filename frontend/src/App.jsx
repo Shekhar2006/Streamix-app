0ccc77b4709ab209608090979fd432635b38d9a4
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-16 23:36:46 +0530

    Create pages and setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
+import toast ,{ Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div className="bg-black-500 h-screen" data-theme="coffee" >
+    <div className="h-screen" data-theme="coffee" >
+      <Routes>
 
-      <button className="btn btn-outline btn-info">Info</button>
-      <button className="btn btn-outline btn-success">Success</button>
-      <button className="btn btn-outline btn-warning">Warning</button>
-      <button className="btn btn-outline btn-error">Error</button>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+
+      </Routes>
+      <Toaster/>
 
     </div>
   )
